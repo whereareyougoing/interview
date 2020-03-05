263d3400e829ef0a65cbb900dff132f09db9136c
song <1007761397@qq.com>
2020-03-06 00:22:10 +0800

    init

---
+++ src/main/java/com/nowcoder/Test.java
@@ -0,0 +1,20 @@
+package com.nowcoder;
+
+/**
+ * @作者 宋艾衡
+ * @日期 2020-03-06
+ * @时间 00:07
+ * @描述
+ */
+public class Test {
+
+    private String name;
+    private String sex;
+
+    public static void main(String[] args) {
+
+        String a = "";
+
+    }
+
+}
